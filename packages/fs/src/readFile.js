fa7f4e5e355d0bc0cd1fd76aeb577420ef605d68
Nicolas Jakob <nicow.jakob@gmail.com>
2017-06-05 17:25:46 +0200

    style(fs): Update arguments types

--- packages/fs/src/readFile.js
+++ packages/fs/src/readFile.js
@@ -3,7 +3,7 @@
 import fs from 'fs';
 
 export default function readFile(filePath: string): Promise<Buffer> {
-  return new Promise((resolve: (Buffer) => void, reject: (Error) => void) => {
+  return new Promise((resolve: (buffer: Buffer) => void, reject: (err: ?Error) => void) => {
     fs.readFile(filePath, (err: ?Error, data: Buffer): void => {
       if (err) {
         return reject(err);
